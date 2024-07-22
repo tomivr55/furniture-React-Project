69a33e2565342283554a32e477ebec7e612c46b4
tomivr55 <tomas.z.minov@gmail.com>
2024-07-22 16:00:23 +0300

    add navigation bar

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,29 @@
+import style from "./Header.module.css";
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="#0">Catalog</a>
+          </li>
+          <li>
+            <a href="#0">Add furniture</a>
+          </li>
+          <li>
+            <a href="#0">Login</a>
+          </li>
+          <li>
+            <a href="#0">Regisrer</a>
+          </li>
+          <li>
+            <a href="#0">Logout</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
